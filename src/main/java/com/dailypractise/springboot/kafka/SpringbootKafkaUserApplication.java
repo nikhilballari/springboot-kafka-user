14fe79c514933f7c9cae08cdcb6117dfdfc9002c
nikhilballari <nikhil.ballari@gmail.com>
2024-04-24 10:53:02 +0100

    spring boot application developed for demonstrating apache kafka consumer functionality

---
+++ src/main/java/com/dailypractise/springboot/kafka/SpringbootKafkaUserApplication.java
@@ -0,0 +1,13 @@
+package com.dailypractise.springboot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailypractise/springboot/kafka/SpringbootKafkaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailypractise.springboot.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
